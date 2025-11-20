d664f4342c680e3664b9c13513e322abd759f87f
John Dave Rojo <rojo.dave2004@gmail.com>
2025-11-20 21:19:17 +0800

    minor changes

--- hapsay360mobile/backend/src/routes/application.js
+++ hapsay360mobile/backend/src/routes/application.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import authMiddleware from "../middleware/authMiddleware.js"; // updated path
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
